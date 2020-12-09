239d20f5bfbf1a8b1ffbd555615587b00776757b
ipenywis <ipenywis@gmail.com>
2020-12-09 23:33:43 +0100

    Remove unused uitls

--- app/utils/size.ts
+++
@@ -1,6 +0,0 @@
-export function bytesToSize(bytes: any): string {
-  var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-  if (bytes == 0) return '0 Byte';
-  var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)) as any);
-  return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i];
-}
